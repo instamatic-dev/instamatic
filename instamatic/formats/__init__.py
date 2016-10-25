26bade5fd2e0f7417924d60888b821109243f0dc
Stef Smeets <stef.smeets@mmk.su.se>
2016-10-25 11:09:29 +0200

    Add function to read_tiff without fabio

--- instamatic/formats/__init__.py
+++ instamatic/formats/__init__.py
@@ -2,10 +2,30 @@ from TiffIO import TiffIO
 import time
 
 def write_tiff(fname, data, header=None):
-    """
-    Overrides the FabioImage.write method and provides a simple TIFF image writer.
-    @param fname: name of the file to save the image to
-    @tag_type fname: string or unicode (file?)...
+    """Simple function to write a tiff file
+
+    fname: str,
+        path or filename to which the image should be saved
+    data: np.ndarray,
+        numpy array containing image data
+    header: dict,
+        dictionary containing the custom tags that should be saved
+        key/value pairs are stored as yaml in the TIFF ImageDescription tag
     """
     tiffIO = TiffIO(fname, mode="w")
     tiffIO.writeImage(data, info=header, software="instamatic", date=time.ctime())
+
+def read_tiff(fname):
+    """Simple function to read a tiff file
+
+    fname: str,
+        path or filename to image which should be opened
+
+    Returns:
+        image: np.ndarray, headerfile: dict
+            a tuple of the image as numpy array and dictionary with all the tem parameters and image attributes
+    """
+    tiff = TiffIO.TiffIO(fname)
+    img = tiff.getImage(0)
+    h = tiff.getInfo(0)
+    return img, h
--- instamatic/tools.py
+++ instamatic/tools.py
@@ -1,5 +1,5 @@
 from scipy import ndimage
-import fabio
+from formats import read_tiff, write_tiff
 import json
 import sys, os
 import numpy as np
@@ -7,7 +7,10 @@ import numpy as np
 def load_img(fn):
     root, ext = os.path.splitext(fn)
     ext = ext.lower()
-    if ext != ".npy":
+    if ext == "tiff" or ext == "tif":
+        arr, h = read_tiff(fn)
+    elif ext != ".npy":
+        import fabio
         arr = fabio.openimage.openimage(fn)
         # workaround to fix headers
         if ext == ".edf":
@@ -28,7 +31,9 @@ def autoscale(img, maxdim=512):
     if maxdim:
         scale = float(maxdim) / max(img.shape)
 
-    return ndimage.zoom(img, scale, order=3), scale
+    return ndimage.zoom(img, scale, order=1), scale
 
 def imgscale(img, scale):
-    return ndimage.zoom(img, scale)
+    if scale == 1:
+        return img
+    return ndimage.zoom(img, scale, order=1)
