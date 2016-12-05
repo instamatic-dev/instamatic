48e913e6cef74bd9a5404ec5a42476518c780658
Stef Smeets <stef.smeets@mmk.su.se>
2016-12-05 18:06:29 +0100

    Add function to control rotation speed of sample stage

--- instamatic/TEMController.py
+++ instamatic/TEMController.py
@@ -464,7 +464,41 @@ class StagePosition(object):
         self._reset(x=True, y=True)
 
     def neutral(self):
-        raise NotImplementedError
+        self.set(x=0, y=0, z=0, a=0, b=0)
+
+    def rotate_to(self, target, start=None, speed=5, delay=0.01, wait_for_stage=False):
+        """
+        Control rotation of the microscope
+
+        target: float
+            final angle in degrees
+        start: float
+            starting angle in degrees (optional)
+        speed: float
+            overall rate of rotation in degrees / second
+        delay: float
+            delay in seconds between updates sent to the microscope (accurate to 10 - 13 milliseconds)
+        """
+        if not start:
+            start = self.a
+        else:
+            self.a = start
+        speed = abs(speed)
+
+        # m equals -1 for positive direction, 1 for negative direction
+        m = cmp(start, target)
+        
+        angle = start
+        
+        t0 = time.time()
+        while cmp(angle, target) == m:
+            t1 = time.time()
+            angle = start - m * (t1 - t0) * speed
+            ctrl.tem.stage3.SetTiltAngle(angle)
+            time.sleep(delay)
+            while wait_for_stage and self._tem.stage3.GetStatus()[3]:  # is stage moving?
+                pass
+            # print t1-t0, angle
 
 
 class TEMController(object):
