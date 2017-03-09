040981b0058cab97221ac0dd2e10cd7419de23fd
Stef Smeets <stef.smeets@mmk.su.se>
2017-03-09 23:29:49 +0100

    First implementation of instamatic.index multiprocessing

--- instamatic/processing/indexer.py
+++ instamatic/processing/indexer.py
@@ -18,6 +18,12 @@ import matplotlib.pyplot as plt
 
 import os, sys, glob
 
+import argparse
+
+__version__ = "2017-03-09"
+__author__ = "Stef Smeets"
+__email__ = "stef.smeets@mmk.su.se"
+
 
 def get_intensities(img, result, projector):
     proj = projector.get_projection(result.alpha, result.beta, result.gamma)
@@ -48,8 +54,8 @@ def standardize_indices(arr, cell, key=None):
 def results2df(results, sort=True):
     """Convert a list of IndexingResult objects to pandas DataFrame"""
     import pandas as pd
-    df = pd.DataFrame(all_results).T
-    df.columns = all_results.values()[0]._fields
+    df = pd.DataFrame(results).T
+    df.columns = results.values()[0]._fields
     if sort:
         df = df.sort_values("score", ascending=False)
     return df
@@ -166,7 +172,7 @@ def get_indices(pks, scale, center, shape, hkl=None):
     i, j = (pks * scale + center).astype(int).T
     sel = (0 < j) & (j < shapey) & (0 < i) & (i < shapex)
     if hkl is None:
-    return i[sel], j[sel]
+        return i[sel], j[sel]
     else:
         return i[sel], j[sel], hkl[sel]
 
@@ -495,11 +501,11 @@ cell:
   spgr: Fm-3c
 data:
   csv_out: results.csv
-  drc_pit: indexing
+  drc_out: indexing
   glob: data/*.tiff
 """
 
-def main():
+def run(arg, chunk=None, dry_run=False):
     if len(sys.argv) == 1:
         print "Usage: instamatic.index indexing.inp"
         print
@@ -508,7 +514,7 @@ def main():
         print TEMPLATE
         exit()
 
-    d = yaml_ordered_load(sys.argv[1])
+    d = yaml_ordered_load(arg)
 
     azimuth   = d["experiment"]["azimuth"]
     stretch   = d["experiment"]["stretch"]
@@ -537,7 +543,16 @@ def main():
 
     fns = glob.glob(file_pat)
     print "Found {} files".format(len(fns))
-    
+
+    if chunk:
+        offset, cpu_count = chunk
+        root, ext = os.path.splitext(csv_out)
+        csv_out = "{}_{}{}".format(root, offset, ext)
+        fns = fns[offset::cpu_count]
+
+    if dry_run:
+        return
+
     if not os.path.exists(drc_out):
         os.mkdir(drc_out)
     
@@ -567,11 +582,168 @@ def main():
 
         np.savetxt(out, hklie, fmt="%4d%4d%4d %7.1f %7.1f")
 
-    write_csv(csv_out, all_refined)
-    
-    print "Writing results to {}".format(csv_out)
+    if len(all_results) > 0:
+        write_csv(csv_out, all_results)
+        print "Writing results to {}".format(csv_out)
     print
     print " >> DONE <<"
+
+
+class ProgressBar(object):
+    """docstring for ProgressBar"""
+    def __init__(self):
+        
+        self.reverse = False
+        self.a = ' '
+        self.b = '='
+        self.width = 10
+        self.delay = 0.2
+
+
+    def loop(self):
+        for i in range(self.width):
+            if self.reverse:
+                i = self.width-i
+            time.sleep(self.delay)
+            printer('['+self.b*(i)+self.a*(self.width-i)+']')
+        self.reverse = not self.reverse
+        for i in range(self.width):
+            if self.reverse:
+                i = self.width-i
+            time.sleep(self.delay)
+            printer('['+self.a*(self.width-i)+self.b*i+']')
+        self.a,self.b = self.b,self.a
+        
+    def clear(self):
+        printer('')
+
+
+def resize_terminal(height=40,width=80):
+    print "\x1b[8;{};{};t".format(height,width)
+
+
+def printer(data):
+    """Print things to stdout on one line dynamically"""
+    sys.stdout.write("\r\x1b[K"+data.__str__())
+    sys.stdout.flush()
+
+
+def merge_csv(csvs):
+    return pd.concat((read_csv(csv) for csv in csvs))
+
+
+def multi_run(arg, procs=1, dry_run=False, resize=False):
+    import subprocess as sp
+    from multiprocessing import cpu_count
+
+    d = yaml_ordered_load(sys.argv[1])
+
+    file_pat  = d["data"]["glob"]
+    csv_out   = d["data"]["csv_out"]
+    drc_out   = d["data"]["drc_out"]
+
+    fns = glob.glob(file_pat)
+    print "Found {} files".format(len(fns))
+
+    csv_outs = []
+    for i in xrange(procs):
+        print " >> Proc #{}: {} files".format(len(fn[i::procs]))
+        root, ext = os.path.splitext(csv_out)
+        csv_outs.append("{}_{}{}".format(root, offset, ext))
+    print
+
+
+    cores = cpu_count()
+
+    assert procs >= 0, 'Expected a positive number of processors'
+    if procs > cores:
+        print '{} cores detected, are you sure you want to run {} processes? [y/n]'.format(cores, procs)
+        confirm = raw_input(' >> [y] ')
+        if 'n' in confirm:
+            sys.exit()
+
+    processes = []
+
+    print 'Starting processes...'
+    for i in xrange(procs):
+        cmd = "instamatic.index.exe {} -c {} {}".format(arg, i, procs)
+
+        print "     >>", cmd,
+
+        if not dry_run:
+            # CREATE_NEW_CONSOLE is windows only
+            p = sp.Popen(cmd, creationflags=sp.CREATE_NEW_CONSOLE)
+            processes.append(p)
+            print 'started'
+        else:
+            print 'not started'
+            
+    if resize: # resize window to fit output
+        resize_terminal(height=5+(i+1)*len(filenames))
+    
+    pb = ProgressBar()
+    while any(p.poll() == None for p in processes):
+        pb.loop()
+    pb.clear()
+
+    all_results = merge_csv(csv_outs)
+    if len(all_results) > 0:
+        write_csv(csv_out, all_results)
+        print "Writing results to {}".format(csv_out)
+    print
+    print " >> Done << "
+
+
+def main():
+    usage = """instamatic.index indexing.inp"""
+
+    description = """
+Program for indexing electron diffraction images.
+
+""" 
+    
+    epilog = 'Updated: {}'.format(__version__)
+    
+    parser = argparse.ArgumentParser(#usage=usage,
+                                    description=description,
+                                    epilog=epilog, 
+                                    formatter_class=argparse.RawDescriptionHelpFormatter,
+                                    version=__version__)
+    
+    parser.add_argument("args", 
+                        type=str, metavar="FILE",
+                        help="Path to input file.")
+
+    parser.add_argument("-j", "--procs", metavar='N',
+                        action="store", type=int, dest="procs",
+                        help="Number of processes to use (default = 1)")
+
+    parser.add_argument("-c", "--chunk", metavar='N', nargs=2,
+                        action="store", type=int, dest="chunk",
+                        help="Used internally to specify the chunk number to process.")
+
+    parser.add_argument("-d", "--dry",
+                        action="store_true", dest="dry_run",
+                        help="Runs the program, but doesn't start any processes.")
+
+    
+    parser.set_defaults(procs=1,
+                        chunk=None,
+                        dry_run=False,
+                        resize=False,
+                        )
+    
+    options = parser.parse_args()
+    arg = options.args
+
+    if not arg:
+        parser.print_help()
+        sys.exit()
+
+    if options.procs > 1:
+        multi_run(arg, procs=options.procs, dry_run=options.dry_run)
+    else:
+        run(arg, options.chunk)
 
 
 if __name__ == '__main__':
