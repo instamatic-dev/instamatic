c02f376fa97a3595e9e461329d30572f8f4a14a4
Stef Smeets <stef.smeets@mmk.su.se>
2017-03-14 14:24:33 +0100

    Plot indexing results if results.ycsv is passed as argument

--- scripts/browser.py
+++ scripts/browser.py
@@ -4,6 +4,11 @@ import os, sys, glob
 import numpy as np
 
 from IPython import embed
+from instamatic.processing.indexer import Indexer, Projector, read_ycsv, get_indices
+
+import argparse
+
+__version__ = "2017-03-12"
 
 
 def get_stage_coords(fns):
@@ -19,8 +24,15 @@ def get_stage_coords(fns):
     # convert to um
     return np.array(coords) / 1000, np.array(has_crystals)
 
-def main():
-    fns = glob.glob("images/image_*.tiff")
+
+def run(filepat="images/image_*.tiff", results=None):
+     # use relpath to normalizes path
+    fns = map(os.path.relpath, glob.glob(filepat))
+
+    if results:
+        df, d = read_ycsv(results)
+        projector = Projector.from_parameters(d["cell"]["params"], d["cell"]["spgr"], d["cell"]["name"], thickness=d["projections"]["thickness"])
+        indexer = Indexer.from_projector(projector, pixelsize=d["experiment"]["pixelsize"])
 
     coords, has_crystals = get_stage_coords(fns)
 
@@ -31,7 +43,9 @@ def main():
     
     ax1 = plt.subplot(131, title="Stage map", aspect="equal")
     # plt_coords, = ax1.plot(coords[:,0], coords[:,1], marker="+", picker=8, c=has_crystals)
-    ax1.scatter(coords[:,0], coords[:,1], marker="+", picker=8, c=has_crystals==False)
+    
+    ax1.scatter(coords[has_crystals==True, 0], coords[has_crystals==True, 1], marker="o", edgecolor="red", facecolor="red")
+    ax1.scatter(coords[:, 0], coords[:, 1], marker=".", color="red", picker=8)
 
     ax1.set_xlabel("Stage X")
     ax1.set_ylabel("Stage Y")
@@ -43,6 +57,7 @@ def main():
     ax3 = plt.subplot(133, title="Diffraction pattern")
     im3 = ax3.imshow(np.zeros_like(img), vmax=250)
     plt_diff, = ax3.plot([], [], "r+", picker=8, lw=0)
+    plt_diff_center, = ax3.plot([], [], "o", lw=0)
 
     def onclick(event):
         click = event.mouseevent.button
@@ -71,6 +86,27 @@ def main():
             im3.set_data(img)
             ax3.set_title(fn_diff)
 
+            if results:
+                r = df.ix[fn_diff]
+                print
+                print r
+                proj = projector.get_projection(r.alpha, r.beta, r.gamma)
+                pks = proj[:,3:5]
+                i, j, hkl = get_indices(pks, r.scale, (r.center_x, r.center_y), img.shape, hkl=proj[:,0:3])
+
+                plt_diff_center.set_xdata(r.center_x)
+                plt_diff_center.set_ydata(r.center_y)
+
+                plt_diff.set_xdata(j)
+                plt_diff.set_ydata(i)
+            else:
+                plt_diff_center.set_xdata([])
+                plt_diff_center.set_ydata([])
+
+                plt_diff.set_xdata([])
+                plt_diff.set_ydata([])
+
+
         elif axes == ax3:
             pass
         
@@ -79,6 +115,44 @@ def main():
     fig.canvas.mpl_connect("pick_event", onclick)
 
     plt.show()
+
+
+def main():
+    usage = """instamatic.browser images/*.tiff -r results.csv"""
+
+    description = """
+Program for indexing electron diffraction images.
+
+""" 
+    
+    epilog = 'Updated: {}'.format(__version__)
+    
+    parser = argparse.ArgumentParser(#usage=usage,
+                                    description=description,
+                                    epilog=epilog, 
+                                    formatter_class=argparse.RawDescriptionHelpFormatter,
+                                    version=__version__)
+    
+    parser.add_argument("args", 
+                        type=str, metavar="FILE",
+                        help="File pattern to image files")
+
+    parser.add_argument("-r", "--results", metavar='RESULTS.csv',
+                        action="store", type=str, dest="results",
+                        help="Path to .csv with results from indexing")
+
+    
+    parser.set_defaults(results=None,
+                        )
+    
+    options = parser.parse_args()
+    arg = options.args
+
+    if not arg:
+        parser.print_help()
+        sys.exit()
+
+    run(filepat=arg, results=options.results)
 
 
 if __name__ == '__main__':
