058db0fd76d0a9a34101b5be3ebb14716bfb5e6b
Stef Smeets <stef.smeets@mmk.su.se>
2017-04-25 18:13:02 +0200

    Fix bugs

--- instamatic/processing/orientations/__init__.py
+++ instamatic/processing/orientations/__init__.py
@@ -1,5 +1,7 @@
 import numpy as np
+import os, sys
 
 def get_orientations(lauegr="-1"):
-    fn = "orientations_{}.txt".format(lauegr.replace("/", "o"))
+    drc = os.path.abspath(os.path.dirname(__file__))
+    fn = os.path.join(drc, "orientations_{}.txt".format(lauegr.replace("/", "o")))
     return np.loadtxt(fn)
--- instamatic/processing/orientations/generate_alphabeta.m
+++ instamatic/processing/orientations/generate_alphabeta.m
@@ -1,5 +1,8 @@
+% generate orientations in terms of X, Y, Z; ALPHA, BETA
+% polar2xyz(ALPHA, BETA) == XYZ
+
 spacing = 1.7;
-syms = {'-1', '2/m', '4/m', 'mmm', '4/mmm', '-3', '6/m', 'm3', '-3m', '6/mmm', 'm3m'};
+syms = {'-1', '2/m', '4/m', 'mmm', '4/mmm', '-3', '6/m', 'm3', '-3m', '6/mmm', 'm3m', 'm-3m'};
 
 for j=1:numel(syms)
     sym = string(syms(j));
@@ -9,8 +12,8 @@ for j=1:numel(syms)
     g = equispacedSO3Grid(cs, ss, 'resolution', spacing*degree);
 
     xyz = g.alphabeta.xyz;
-    rho = g.alphabeta.rho;
-    theta = g.alphabeta.theta;
+    theta = g.alphabeta.theta; % alpha
+    rho = g.alphabeta.rho;     % beta
 
     strsym = strrep(sym, '/', 'o');
     fn = sprintf('orientations_%s.txt', strsym);
