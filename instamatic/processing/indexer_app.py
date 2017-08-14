82c251787ac7c21bf9d3ca405c29f7aee5dc3ad7
Stef Smeets <stef.smeets@mmk.su.se>
2017-08-14 10:10:29 +0200

    results.ycsv file can now be used as input for filelist

--- instamatic/processing/indexer_app.py
+++ instamatic/processing/indexer_app.py
@@ -64,8 +64,13 @@ def copy_template():
 def get_files(file_pat):
     """Grab files from globbing pattern or stream file"""
     if os.path.exists(file_pat):
-        f = open(file_pat, "r")
-        fns = [line.split("#")[0].strip() for line in f if not line.startswith("#")]
+        root, ext = os.path.splitext(file_pat)
+        if ext.lower() == ".ycsv":
+            df, d = read_ycsv(file_pat)
+            fns = df.index.tolist()
+        else:
+            f = open(file_pat, "r")
+            fns = [line.split("#")[0].strip() for line in f if not line.startswith("#")]
     else:
         fns = glob.glob(file_pat)
 
