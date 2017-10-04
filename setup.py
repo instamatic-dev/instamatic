9e810fa0e3d3d3a5c600be07162355e8199cd138
Stef Smeets <stef.smeets@mmk.su.se>
2017-10-04 13:14:35 +0200

    Remove unused entry_points from setup.py and update requirements

--- setup.py
+++ setup.py
@@ -25,7 +25,9 @@ setup(
               "instamatic.processing",
               "instamatic.formats"],
 
-    install_requires=["numpy", "comtypes", "scikit-image", "pyyaml", "lmfit"],
+    install_requires=["numpy", "comtypes", "scipy", "scikit-image", "pyyaml", "lmfit"],
+
+    extras_require=["pandas", "matplotlib", "fabio"],
 
     package_data={
         "": ["LICENCE",  "readme.md", "setup.py"],
@@ -37,16 +39,8 @@ setup(
             'instamatic                               = instamatic.experiment:main',
             'instamatic.gui                           = instamatic.gui:main',
             # experiment
-            'instamatic.find_crystals                 = instamatic.find_crystals:find_crystals_entry',
-            'instamatic.find_holes                    = instamatic.find_holes:find_holes_entry',
             'instamatic.camera                        = instamatic.camera.camera:main_entry',
             'instamatic.controller                    = instamatic.TEMController.TEMController:main_entry',
-            'instamatic.viewer                        = instamatic.viewer:main',
-            'instamatic.map_holes                     = instamatic.app:map_holes_on_grid_entry',
-            'instamatic.goto_hole                     = instamatic.app:goto_hole_entry',
-            'instamatic.prepare_experiment            = instamatic.app:prepare_experiment_entry',
-            'instamatic.plot_experiment               = instamatic.app:plot_experiment_entry',
-            'instamatic.update_experiment_with_coords = instamatic.app:update_experiment_with_hole_coords_entry',
             # calibrate
             'instamatic.calibrate_stage_lowmag        = instamatic.calibrate.calibrate_stage_lowmag:main_entry',
             'instamatic.calibrate_stage_mag1          = instamatic.calibrate.calibrate_stage_mag1:main_entry',
@@ -55,11 +49,6 @@ setup(
             'instamatic.flatfield                     = instamatic.flatfield:main_entry',
             # processing
             'instamatic.stretch_correction            = instamatic.processing.stretch_correction:main',
-            'instamatic.process                       = instamatic.processing.process_images:main',
-            'instamatic.hitfinder                     = scripts.hitfinder:main',
-            'instamatic.index                         = instamatic.processing.indexer_app:main',
-            'instamatic.serialmerge                   = instamatic.processing.serialmerge:serialmerge_entry',
-            'instamatic.mergeprep                     = scripts.mergeprep:main',
             # explore
             'instamatic.browser                       = scripts.browser:main',
         ]
