01b02061b894f8d248bc71a35ddbb07b61d87cc7
Stef Smeets <stef.smeets@mmk.su.se>
2017-10-05 09:38:30 +0200

    Make start/stop buttons inactive depending on state of the experiment

--- instamatic/gui/cREDframe.py
+++ instamatic/gui/cREDframe.py
@@ -10,28 +10,17 @@ class ExperimentalcRED(LabelFrame):
         self.init_vars()
 
         Label(self, width=30,text="Exposure time").grid(row=4, column=0)
-        self.exposure_time = Entry(self, width=50, textvariable=self.var_exposure_time)
+        self.exposure_time = Entry(self, width=70, textvariable=self.var_exposure_time)
         self.exposure_time.grid(row=4, column=1)
 
         self.CollectionButton = Button(self, text="Start Collection", command=self.start_collection, anchor=W)
         self.CollectionButton.grid(row=10, column=0)
 
-        self.CollectionStopButton = Button(self, text="Stop Collection", command=self.stop_collection, anchor=W)
+        self.CollectionStopButton = Button(self, text="Stop Collection", command=self.stop_collection, anchor=W, state=DISABLED)
         self.CollectionStopButton.grid(row=11 , column=0)
 
-        self.CollectionContButton = Button(self, text="Continue Collection", command=self.continue_collection, anchor=W)
-        self.CollectionContButton.grid(row=12, column=0)
-        
-        self.lb_coll1=Label(self,width=60,text="Now you can start to rotate the goniometer at any time.")
-        self.lb_coll2=Label(self,width=60,text="Click STOP COLLECTION BEFORE removing your foot from the pedal!")
-        self.lb_coll1.grid(row=6,column=0)
-        self.lb_coll2.grid(row=7,column=0)
-        self.lb_coll1.grid_forget()
-        self.lb_coll2.grid_forget()
-
-        self.lb_coll3=Label(self,width=60,text="Please remember to change the experiment folder to continue collection!")
-        self.lb_coll3.grid(row=6,column=0)
-        self.lb_coll3.grid_forget()
+        self.lb_coll1 = Label(self, width=60, text="Now you can start to rotate the goniometer at any time.", fg="red")
+        self.lb_coll2 = Label(self, width=60, text="Click STOP COLLECTION BEFORE removing your foot from the pedal!", fg="red")
         
     def init_vars(self):
         self.var_exposure_time = DoubleVar(value=0.5)
@@ -45,30 +34,22 @@ class ExperimentalcRED(LabelFrame):
 
     def start_collection(self):
         print "Start button pressed"
-        # TODO: make button inactive when it is clicked
-        self.lb_coll3.grid_forget()
-        self.lb_coll1.grid(row=6,column=0)
-        self.lb_coll2.grid(row=7,column=0)
+        # TODO: make a pop up window with the STOP button?
+        self.CollectionStopButton.config(state=NORMAL)
+        self.CollectionButton.config(state=DISABLED)
+        self.lb_coll1.grid(row=10, column=1)
+        self.lb_coll2.grid(row=11, column=1)
         self.startEvent.set()
         self.triggerEvent.set()
 
     def stop_collection(self):
         print "Stop button pressed"
-        # TODO: make button active only during experiment
+        self.CollectionStopButton.config(state=DISABLED)
+        self.CollectionButton.config(state=NORMAL)
         self.lb_coll1.grid_forget()
         self.lb_coll2.grid_forget()
         self.stopEvent.set()
-        
-    def continue_collection(self):
-        print "Continue button pressed"
-        # NOTE: why is this button necessary?
-        self.lb_coll1.grid_forget()
-        self.lb_coll2.grid_forget()
-        self.lb_coll3.grid(row=6,column=0)
-        self.stopEvent.clear()
-        self.startEvent.clear()
-        self.triggerEvent.clear()
-        
+
     def get_expt(self):
         return self.var_exposure_time.get()
 
