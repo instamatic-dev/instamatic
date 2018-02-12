0a7dc9bf47f2f38b11e19e8dcb6609f1ddcd40d6
Stef Smeets <stef.smeets@mmk.su.se>
2018-02-12 16:13:24 +0100

    Update config file

--- instamatic/config/__init__.py
+++ instamatic/config/__init__.py
@@ -1,18 +1,22 @@
 import os
 import yaml
+from pathlib import Path
 
 import logging
 logger = logging.getLogger(__name__)
 
-config_dir = os.path.join(os.environ["AppData"], "instamatic")
-
-if not os.path.exists(config_dir):
-    logger.info("Created config directory:", config_dir)
-    os.makedirs(config_dir)
-
-search_drcs = (config_dir, os.path.dirname(__file__)) 
 # print "config dir: ", config_dir
 # print "search dirs:", search_drcs
+
+def get_base_drc():
+    portable = Path(os.environ["instamatic"])  # if installed in portable way
+    app_data = Path(os.environ["AppData"])
+
+    search_drcs = (portable, app_data / "instamatic", Path(__file__).parents[0]) 
+
+    for drc in search_drcs:
+        if drc.exists():
+            return drc
 
 
 class ConfigObject(object):
@@ -24,19 +28,24 @@ class ConfigObject(object):
         for key, value in d.items():
             setattr(self, key, value)
 
-
-def get_config(name, drc=""):
-    fn = os.path.join(drc, "{}.yaml".format(name))
-    for drc in search_drcs:
-        config = os.path.join(drc, fn)
-        if os.path.exists(config):
-            break
-    return ConfigObject(yaml.load(open(config, "r")))
+    @classmethod
+    def from_file(cls, path):
+        return cls(yaml.load(open(path, "r")))
 
 
-cfg = get_config("global")
+base_drc = get_base_drc()
 
-microscope = get_config(cfg.microscope, "microscope")
-calibration = get_config(cfg.calibration, "calibration")
-camera = get_config(cfg.camera, "camera")
+assert base_drc.joinpath("config").exists()
+print(f"(Instamatic base directory: {base_drc}")
 
+cfg = ConfigObject.from_file(base_drc / "config" / "global.yaml")
+
+microscope = ConfigObject.from_file(base_drc / "config" / cfg.microscope / "microscope.yaml")
+calibration = ConfigObject.from_file(base_drc / "config" / cfg.calibration / "calibration.yaml")
+camera = ConfigObject.from_file(base_drc / "config" / cfg.camera / "camera.yaml")
+
+scripts_drc = base_drc / "scripts"
+logs_drc = base_drc / "logs"
+
+scripts_drc.mkdir(exist_ok=True)
+logs_drc.mkdir(exist_ok=True)
