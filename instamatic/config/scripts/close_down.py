a207dcc6281e5bceb1e6936daa75c72282895956
Stef Smeets <s.smeets@tudelft.nl>
2019-05-17 09:16:01 +0200

    Add example script to close down microscope

---
+++ instamatic/config/scripts/close_down.py
@@ -0,0 +1,7 @@
+controller.ctrl.stageposition.neutral()
+controller.ctrl.mode = "mag1"
+controller.ctrl.brightness.max()
+controller.ctrl.magnification.value = 10000
+controller.ctrl.spotsize = 1
+controller.ctrl.screen_down()
+print("All done!")
