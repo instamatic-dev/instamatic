dead6e799187042ad05ed52a782bf2e7f5132b52
Stef Smeets <s.smeets@tudelft.nl>
2019-07-22 14:32:55 +0200

    Add functions to add markers to map item and save .nav file

--- instamatic/serialem.py
+++ instamatic/serialem.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import re
 import numpy as np
+import random
 
 
 # int
@@ -37,6 +38,9 @@ class NavItem(object):
         1: Polygon
         2: Map
     """
+
+    TAG_ID_ITERATOR = 1
+    
     def __init__(self, d: dict, tag: str):
         super().__init__()
         self._keys = tuple(d.keys())
@@ -44,6 +48,10 @@ class NavItem(object):
         self.Acquire = 0
         self.__dict__.update(d)
         
+        if not tag:
+            tag = f"Item-{NavItem.TAG_ID_ITERATOR}"
+            NavItem.TAG_ID_ITERATOR += 1
+
         self.tag = tag
 
     def __repr__(self):
@@ -69,12 +77,6 @@ class NavItem(object):
     def stage_xy(self) -> list:
         return self.StageXYZ[0:2]
 
-    @property
-    def map_scale_matrix(self) -> np.array:
-        MapBinning = self.MapBinning
-        MontBinning = self.MontBinning
-        return (MapBinning / MontBinning) * np.array(self.MapScaleMat).reshape(2, 2)
-
     def to_string(self) -> str:
         """Convert nav item to string that can be printed to .nav file"""
         s = f"[Item = {self.tag}]\n"
@@ -84,14 +86,18 @@ class NavItem(object):
         for key in sorted(d.keys()):
             val = d[key]
 
-            if key in int_map:
-                val = str(val)
-            elif key in float_map:
-                val = str(val)
-            elif key in list_float_map:
-                val = " ".join([str(x) for x in val])
-            elif key in list_int_map:
-                val = " ".join([str(x) for x in val])
+            try:
+                if key in int_map:
+                    val = str(val)
+                elif key in float_map:
+                    val = str(val)
+                elif key in list_float_map:
+                    val = " ".join([str(x) for x in val])
+                elif key in list_int_map:
+                    val = " ".join([str(x) for x in val])
+            except TypeError as e:
+                print(e)
+                print(key, val)
 
             s += f"{key} = {val}\n"
 
@@ -107,6 +113,14 @@ class NavItem(object):
 class MapItem(NavItem):
     """Adds some extra methods for map items"""
     
+    GROUP_ID_ITERATOR = random.randint(1, 90000)
+
+    @property
+    def map_scale_matrix(self) -> np.array:
+        MapBinning = self.MapBinning
+        MontBinning = self.MontBinning
+        return (MapBinning / MontBinning) * np.array(self.MapScaleMat).reshape(2, 2)
+
     def pixel_to_stagecoords(self, coords: list) -> np.array:
         """Convert from pixel coordinates to stage coordinates"""
         coords = np.array(coords)
@@ -149,9 +163,50 @@ class MapItem(NavItem):
         plt.matshow(im, vmax=np.percentile(im, 99))
         yres = self.MapWidthHeight[1]
         for marker in markers:
-            xy = np.array([marker.stage_x, marker.stage_y])
-            px, py = self.stage_to_pixelcoords(xy)
-            plt.plot(px, yres - py, "ro", markerfacecolor='none', markersize=20, markeredgewidth=2)
+            if isinstance(marker, NavItem):
+                xy = np.array([marker.stage_x, marker.stage_y])
+                px, py = self.stage_to_pixelcoords(xy)
+                py = yres - py
+            else:
+                py, px = marker
+    
+            plt.plot(px, py, "ro", markerfacecolor='none', markersize=20, markeredgewidth=2)
+
+    def add_marker(self, coord, tag=None, acquire=True) -> "NavItem":
+        """Add pixel coordinate (numpy) as marker to a map item"""
+
+        # assuming pixel coords from numpy
+        py, px = coord
+        yres = self.MapWidthHeight[1]
+        py = yres - py
+        stage_x, stage_y = self.pixel_to_stagecoords((px, py))
+
+        d = {}
+        d["BklshXY"] = self.BklshXY
+        d["Color"] = 0
+        d["DrawnID"] = self.MapID
+        d["GroupID"] = MapItem.GROUP_ID_ITERATOR
+        # d["MapID"] = 1 + self.i  # must be a unique ID under 100000 [optional, default=0]
+        d["Acquire"] = int(acquire)
+        d["NumPts"] = 1
+        d["PtsX"] = [stage_x]
+        d["PtsY"] = [stage_y]
+        d["Regis"] = self.Regis
+        d["StageXYZ"] = [stage_x, stage_y, self.stage_z]
+        d["Type"] = 0
+        return NavItem(d, tag=tag)
+
+    def add_marker_group(self, coords, acquire=True) -> list:
+        """Add pixel coordinates (numpy) as markers to a map item"""
+        ret = []
+        for i, coord in enumerate(coords):
+            tag = f"{self.tag}-{i}"
+            item = self.add_marker(coord, tag=tag, acquire=True)
+            ret.append(item)
+
+        MapItem.GROUP_ID_ITERATOR += 1
+
+        return ret
 
 
 def block2dict(block: list) -> dict:
@@ -203,10 +258,13 @@ def block2nav(block: list, tag=None) -> "NavItem":
     return ret 
 
 
-def read_nav_file(fn: str) -> list:
+def read_nav_file(fn: str, acquire_only: bool=False) -> list:
     """
     Reads a SerialEM .nav file and returns a list of dictionaries
     containing nav item data.
+
+    acquire_only: bool
+        read only files with the Acquire tag set
     """
 
     # https://regex101.com/
@@ -238,7 +296,31 @@ def read_nav_file(fn: str) -> list:
         else:
             print(line)
 
+    if acquire_only:
+        items = [item for item in items if item.Acquire]
+
     return items
+
+
+def write_nav_file(fn: str, *items, mode="w") -> None:
+    """
+    Write list of nav items to a navigator file with filename `fn` to be read by SerialEM
+
+    `items` must be a list of NavItem / MapItem objects
+
+    mode can be "w" to write a new file, or "a" to append
+    items to an existing file
+    """
+    f = open(fn, mode) if fn else None
+    version = "2.00"
+    
+    if mode == "w":
+        print(f"AdocVersion = {version}", file=f)
+        print(f"LastSavedAs = {fn}", file=f)
+        print("", file=f)
+    
+    for item in items:
+        print(item.to_string(), file=f)
 
 
 if __name__ == '__main__':
