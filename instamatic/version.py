a70315d4b391d97e62fe7ca576f38c95a668a40f
Stef Smeets <s.smeets@tudelft.nl>
2019-10-02 17:32:18 +0200

    Bump version for release

--- instamatic/version.py
+++ instamatic/version.py
@@ -7,7 +7,7 @@
 #                                                       #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-tag = "dev"
+tag = ""
 VERSION = (1, 1, 0, tag)
 
 __version__ = "{}.{}.{}{}".format(*VERSION, tag)
@@ -15,14 +15,14 @@ __title__ = "instamatic"
 __long_title__ = f"{__title__} v{__version__}"
 __author__ = "Stef Smeets"
 __author_email__ = "s.smeets@tudelft.nl"
-__description__ = "Python program to collect serial and rotation electron diffraction data"
+__description__ = "Python program for automated serial electron diffraction data collection"
 __license__ = "GPLv3"
 __url__ = "http://github.com/stefsmeets/instamatic"
-__doi__ = "https://doi.org/10.5281/zenodo.2026774"
-__citation__ = f"Instamatic (Version 1.0), Zenodo (2018), {__doi__}"
+__doi__ = "https://doi.org/10.5281/zenodo.1090388"
+__citation__ = f"Instamatic (Version 1.1), Zenodo (2019), {__doi__}"
 __citation_cred__ = "J. Appl. Cryst. (2018). 51, 1652–1661, https://doi.org/10.1107/S1600576718015145"
 __citation_serialed__ = "J. Appl. Cryst. (2018). 51, 1262–1273, https://doi.org/10.1107/S1600576718009500"
-
+__citation_serialred__ = "IUCrJ. (2019). 6(5), 854-867, https://doi.org/10.1107/S2052252519007681"
 
 def register_thank_you_message():
     import atexit
