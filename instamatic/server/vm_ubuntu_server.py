92bdd766496f9e262806b69867f34cbc1cf72252
binwang <bin.wang@vironova.com>
2019-12-23 11:34:24 +0100

    running xds possibility using virtualbox server under windows 7

---
+++ instamatic/server/vm_ubuntu_server.py
@@ -0,0 +1,50 @@
+import virtualbox
+import time
+
+def start_vm_process(vmname="Ubuntu 14.04.3", vmachine_pwd="testtest"):
+	try:
+		vbox = virtualbox.VirtualBox()
+	except ModuleNotFoundError:
+		print("Please install virtualbox with guest ubuntu and virtualbox SDK first before using this server.")
+		print("Download virtualbox python SDK from:")
+		print("https://www.virtualbox.org/wiki/Downloads")
+
+	session = virtualbox.Session()
+	machine = vbox.find_machine(vmname)
+	progress = machine.launch_vm_progress(session, "gui", "")
+	progress.wait_for_completion()
+
+	"""wait for 15 s to ensure a normal vm startup"""
+	time.sleep(15)
+
+	"""assume vm is password protected"""
+	session.console.keyboard.put_keys(vmachine_pwd)
+	session.console.keyboard.put_keys(["ENTER"])
+	return session
+
+
+def vm_ubuntu_start_terminal(session):
+	"""type ctrl+alt+t to bring up terminal in the ubuntu machine"""
+	session.console.keyboard.put_keys(press_keys="t", hold_keys=["CTRL","ALT"])
+
+def vm_ubuntu_start_xds_AtFolder(session, path= "/media/sf_SharedWithVM/test_vm_server"):
+	session.console.keyboard.put_keys("cd ")
+	session.console.keyboard.put_keys("{}".format(path))
+	session.console.keyboard.put_keys(["ENTER"])
+
+	session.console.keyboard.put_keys("xds")
+	session.console.keyboard.put_keys(["ENTER"])
+
+def close_down_vm_process(session):
+	session.console.power_down()
+
+def main():
+	print("Starting Ubuntu server installed in VirtualBox...")
+	session = start_vm_process()
+
+	vm_ubuntu_start_terminal(session)
+	vm_ubuntu_start_xds_AtFolder(session)
+	close_down_vm_process(session)
+
+if __name__ == '__main__':
+    main()
