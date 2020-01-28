6a94d6b67e7c71eed424fbd35e31fd56629624fb
Stef Smeets <s.smeets@tudelft.nl>
2020-01-28 11:29:42 +0100

    Add difffocus button to control frame

--- instamatic/gui/ctrl_frame.py
+++ instamatic/gui/ctrl_frame.py
@@ -82,6 +82,17 @@ class ExperimentalCtrl(LabelFrame):
         b_stage_get = Button(frame, text='Get', command=self.get_stage)
         b_stage_get.grid(row=6, column=4, sticky='W')
 
+        # defocus button
+        Label(frame, text='Diff defocus:', width=20).grid(row=13, column=0, sticky='W')
+        self.e_diff_defocus = Spinbox(frame, textvariable=self.var_diff_defocus, width=10, from_=-10000, to=10000, increment=100)
+        self.e_diff_defocus.grid(row=13, column=1, sticky='EW')
+
+        self.c_toggle_defocus = Checkbutton(frame, text='Toggle defocus', variable=self.var_toggle_diff_defocus, command=self.toggle_diff_defocus)
+        self.c_toggle_defocus.grid(row=13, column=2, sticky='W', columnspan=2)
+
+        self.b_reset_defocus = Button(frame, text='Reset', command=self.reset_diff_defocus, state=DISABLED)
+        self.b_reset_defocus.grid(row=13, column=4, sticky='EW')
+
         # frame.grid_columnconfigure(1, weight=1)
         frame.pack(side='top', fill='x', padx=10, pady=10)
 
@@ -138,6 +149,9 @@ class ExperimentalCtrl(LabelFrame):
 
         self.var_brightness = IntVar(value=65535)
         self.var_difffocus = IntVar(value=65535)
+
+        self.var_diff_defocus = IntVar(value=1500)
+        self.var_toggle_diff_defocus = BooleanVar(value=False)
 
         self.var_stage_wait = BooleanVar(value=True)
 
@@ -229,6 +243,24 @@ class ExperimentalCtrl(LabelFrame):
     def find_eucentric_height(self):
         self.q.put(('ctrl', {'task': 'find_eucentric_height'}))
         self.triggerEvent.set()
+
+    def toggle_diff_defocus(self):
+        toggle = self.var_toggle_diff_defocus.get()
+
+        if toggle:
+            offset = self.var_diff_defocus.get()
+            self.ctrl.difffocus.defocus(offset=offset)
+            self.b_reset_defocus.config(state=NORMAL)
+        else:
+            self.ctrl.difffocus.refocus()
+            self.var_toggle_diff_defocus.set(False)
+
+        self.get_difffocus()
+
+    def reset_diff_defocus(self):
+        self.ctrl.difffocus.refocus()
+        self.var_toggle_diff_defocus.set(False)
+        self.get_difffocus()
 
 
 module = BaseModule(name='ctrl', display_name='control', tk_frame=ExperimentalCtrl, location='bottom')
