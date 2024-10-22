9cac10c99e4004cf59397411e5b4199b00b7af09
Viljar Femoen <viljar.femoen@hotmail.no>
2024-10-22 09:54:08 +0200

    Properly separate read/write tests

--- tests/test_formats.py
+++ tests/test_formats.py
@@ -19,7 +19,7 @@ def header():
     return {"value": 123, "string": "test"}
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def temp_data_file(tmp_path_factory) -> str:
     return str(tmp_path_factory.mktemp("data", numbered=True) / "out.")
 
@@ -28,7 +28,6 @@ def temp_data_file(tmp_path_factory) -> str:
     ["format", "write_func", "with_header"],
     [
         ("tiff", formats.write_tiff, True),
-        ("img", formats.write_cbf, True),
         ("cbf", formats.write_cbf, True),
         ("mrc", formats.write_mrc, False),
         ("smv", formats.write_adsc, True),
@@ -60,22 +59,26 @@ def test_write_rename_ext(format, write_func, alt, data, header, temp_data_file)
     assert os.path.exists(out_alt)
 
 
-@pytest.mark.depends(on=["test_write"])
 @pytest.mark.parametrize(
-    ["format", "raises"],
+    ["format", "write_func", "with_header", "raises"],
     [
-        ("tiff", does_not_raise()),
-        ("smv", does_not_raise()),
-        ("img", does_not_raise()),
-        ("h5", does_not_raise()),
+        
+        ("tiff", formats.write_tiff, True, does_not_raise()),
+        ("smv", formats.write_adsc, True, does_not_raise()),
+        ("img", formats.write_adsc, True, does_not_raise()),
+        ("h5", formats.write_hdf5, True, does_not_raise()),
         # Header is not supported
-        ("mrc", pytest.raises(ValueError, match="Header mismatch")),
-        ("cbf", pytest.raises(NotImplementedError)),
-        ("invalid_extension", pytest.raises(OSError)),
-        ("does_not_exist.h5", pytest.raises(FileNotFoundError)),
+        ("mrc", formats.write_mrc, False, pytest.raises(ValueError, match="Header mismatch")),
+        ("cbf", formats.write_cbf, True, pytest.raises(NotImplementedError)),
+        ("invalid_extension", lambda *args: None, False, pytest.raises(OSError)),
+        ("does_not_exist.h5", lambda *args: None, False, pytest.raises(FileNotFoundError)),
     ],
 )
-def test_read(format, raises, data, header, temp_data_file):
+def test_read(format, write_func, with_header, raises, data, header, temp_data_file):
+    # Generate file
+    out = temp_data_file + format
+    write_func(out, data, header if with_header else None)
+
     with raises:
         out = temp_data_file + format
 
@@ -83,5 +86,7 @@ def test_read(format, raises, data, header, temp_data_file):
 
         assert np.allclose(img, data)
         assert isinstance(h, dict)
+
+        # Check if the header we want is in the header we read
         if not all(str(v) == str(h.get(k)) for k, v in header.items()):
             raise ValueError("Header mismatch")
