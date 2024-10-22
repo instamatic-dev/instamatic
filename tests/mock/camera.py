f7adf0bf93305721418236c7603d2e0e0ae5b622
Viljar Femoen <viljar.femoen@hotmail.no>
2024-10-22 09:54:09 +0200

    Add rudimentary camera tests

--- tests/mock/camera.py
+++ tests/mock/camera.py
@@ -31,6 +31,7 @@ __all__ = [
 
 
 class CameraMockBase:
+    """Override `load_defaults` as the config file for each microscope is not present"""
     def load_defaults(self):
         for key, val in config.camera.mapping.items():
             setattr(self, key, val)
@@ -41,8 +42,6 @@ class CameraGatan2Mock(CameraMockBase, CameraGatan2):
         self.name = name
         self.g = GatanSocketMock(port="")
         self._recording = False
-        self.load_defaults()
-
 
 class CameraMerlinMock(CameraMockBase, CameraMerlin):
     host = "127.0.0.1"
@@ -61,11 +60,18 @@ class CameraSimuMock(CameraMockBase, CameraSimu):
 
 
 class CameraDLLMock(CameraMockBase, CameraDLL):
-    pass
+    def establish_connection(self) -> None:
+        # The connection opens a window.
+        # Currently no simple way to close it, as it halts execution.
+        # return super().establish_connection()
+        raise NotImplementedError()
 
 
 class CameraTPXMock(CameraMockBase, CameraTPX):
-    pass
+    def acquire_lock(self):
+        from pathlib import Path
+        self.lockfile = Path(__file__).with_name("timepix_mock.lockfile")
+        return super().acquire_lock()
 
 
 class CameraEMMENUMock(CameraMockBase, CameraEMMENU):
--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,3 +1,24 @@
+import pytest
+
+from instamatic.camera.camera_base import CameraBase
+from instamatic.camera.camera_emmenu import CameraEMMENU
+from instamatic.camera.camera_gatan import CameraDLL
+from instamatic.camera.camera_gatan2 import CameraGatan2
+from instamatic.camera.camera_merlin import CameraMerlin
+from instamatic.camera.camera_simu import CameraSimu
+from instamatic.camera.camera_timepix import CameraTPX
+
+from .mock.camera import (
+    CameraDLLMock,
+    CameraEMMENUMock,
+    CameraGatan2Mock,
+    CameraMerlinMock,
+    CameraServalMock,
+    CameraSimuMock,
+    CameraTPXMock,
+)
+
+
 def test_get_image(ctrl):
     bin1 = 1
     bin2 = 2
@@ -22,3 +43,35 @@ def test_functions(ctrl):
     dims = ctrl.cam.get_image_dimensions()
     assert isinstance(dims, tuple)
     assert len(dims) == 2
+
+
+@pytest.mark.parametrize(
+    "cam",
+    [
+        pytest.param(CameraDLLMock, marks=pytest.mark.xfail(reason="establish_connection opens a popup window which halts execution")),
+        pytest.param(CameraEMMENUMock, marks=pytest.mark.xfail(reason="Not implemented")),
+        CameraGatan2Mock,
+        CameraMerlinMock,
+        pytest.param(CameraServalMock, marks=pytest.mark.xfail(reason="Not implemented")),
+        CameraSimuMock,
+        CameraTPXMock,
+    ],
+)
+def test_init_mock(cam):
+    c = cam()
+
+@pytest.mark.parametrize(
+        "cam",
+        [
+            CameraSimu,
+            pytest.param(CameraDLL, marks=pytest.mark.xfail(reason="Needs config")),
+            pytest.param(CameraGatan2, marks=pytest.mark.xfail(reason="Needs config + server")),
+            CameraTPX,
+            pytest.param(CameraEMMENU, marks=pytest.mark.xfail(reason="WinError: Invalid class string")),
+            pytest.param(CameraMerlin, marks=pytest.mark.xfail(reason="Needs config + server")),
+        ]
+)
+def test_init(cam):
+    # Use "test" as the name of the camera, as this is where the settings are read from
+    c = cam(name="test")
+    assert isinstance(c, CameraBase)
