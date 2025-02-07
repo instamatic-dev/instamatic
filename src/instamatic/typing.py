2899eef4c0022185b14d657fd38fa0857252b2f2
Daniel Tchoń <dantch@onet.eu>
2025-02-07 13:41:42 +0100

    Make `int_nm`, `float_deg` just `int`, `float` on Python3.8- that don't have `Annotated`

--- src/instamatic/typing.py
+++ src/instamatic/typing.py
@@ -1,6 +1,12 @@
 from __future__ import annotations
 
-from typing import Annotated
+import sys
 
-int_nm = Annotated[int, 'Length expressed in nanometers']
-float_deg = Annotated[float, 'Angle expressed in degrees']
+if sys.version_info >= (3, 9):
+    from typing import Annotated
+
+    int_nm = Annotated[int, 'Length expressed in nanometers']
+    float_deg = Annotated[float, 'Angle expressed in degrees']
+else:
+    int_nm = int
+    float_deg = float
