6328f1330d2fd1724fcff1b2c61b8acf80268cd3
Daniel Tchoń <dantch@onet.eu>
2025-03-21 14:44:25 +0100

    `VideoStream`: Generalize real & fake streams under common base class

--- src/instamatic/gui/videostream_frame.py
+++ src/instamatic/gui/videostream_frame.py
@@ -9,10 +9,9 @@ import numpy as np
 from PIL import Image, ImageTk
 
 import instamatic.gui.videostream_service as vss_module
+from instamatic.gui.base_module import BaseModule
 from instamatic.gui.click_dispatcher import ClickDispatcher
 from instamatic.utils.spinbox import Spinbox
-
-from .base_module import BaseModule
 
 
 class VideoStreamFrame(LabelFrame):
@@ -324,9 +323,9 @@ def ipy_embed(*args, **kwargs):
 
 if __name__ == '__main__':
     from instamatic import config
-    from instamatic.camera import VideoStream
+    from instamatic.camera import LiveVideoStream
 
-    stream = VideoStream(cam=config.camera.name)
+    stream = LiveVideoStream(cam=config.camera.name)
 
     if False:
         threading.Thread(target=ipy_embed).start()
