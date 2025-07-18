9e32855dc40214f24471c80f743d9af67354a9fa
Daniel Tchoń <dantch@onet.eu>
2025-07-18 17:53:48 +0200

    Remove debug timing instructions

--- src/instamatic/gui/videostream_frame.py
+++ src/instamatic/gui/videostream_frame.py
@@ -15,7 +15,6 @@ from instamatic.formats import read_tiff, write_tiff
 from instamatic.gui.base_module import BaseModule
 from instamatic.gui.click_dispatcher import ClickDispatcher
 from instamatic.gui.videostream_processor import VideoStreamProcessor
-from instamatic.performance_tracker import PerformanceTracker
 from instamatic.processing.flatfield import apply_flatfield_correction
 from instamatic.utils.spinbox import Spinbox
 
@@ -239,7 +238,6 @@ class VideoStreamFrame(LabelFrame):
         self.stream.update_frametime(self.frametime)
         self.after(500, self.on_frame)
 
-    # @PerformanceTracker('150725_new_1000reds')
     def on_frame(self, event=None):
         """Get the newest image from `processor`, adapt to GUI and display."""
         if self.frame is not None:
