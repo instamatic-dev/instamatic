d2bfb1c682501a29ab5ff1ab8830a1109bf2c9f2
Daniel Tchoń <dantch@onet.eu>
2025-10-14 17:51:56 +0200

    Rephrase `VideoStreamProcessor.temporary` using `blocked` context

--- src/instamatic/gui/videostream_processor.py
+++ src/instamatic/gui/videostream_processor.py
@@ -167,16 +167,15 @@ class VideoStreamProcessor:
         """
         pre_context_values = self.temporary_frame, self.temporary_image
         try:
-            self.vsf.stream.block()
             if frame is not None:
                 self.temporary_frame = frame
             if image is not None:
                 self.temporary_image = image
             elif figure is not None:
                 self.temporary_image = self.render_figure(figure)
-            yield
+            with self.vsf.stream.blocked():
+                yield
         finally:
-            self.vsf.stream.unblock()
             self.temporary_frame, self.temporary_image = pre_context_values
 
     @property
