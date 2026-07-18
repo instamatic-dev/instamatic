7e35ec53c6cbd3caaa57ea67893308f28d025f5c
Daniel Tchoń <dantch@onet.eu>
2026-07-18 12:14:19 +0200

    Pre-set starting beamshift before experiment to avoid issues with first frame

--- src/instamatic/experiments/fast_adt/experiment.py
+++ src/instamatic/experiments/fast_adt/experiment.py
@@ -137,7 +137,10 @@ class Run:
 
     @property
     def has_beamshifts(self) -> bool:
-        return {'beamshift_x', 'beamshift_y'}.issubset(self.table.columns)
+        columns = ['beamshift_x', 'beamshift_y']
+        columns_exist = set(columns).issubset(self.table.columns)
+        columns_have_nans = self.table[columns].isna().any().any()
+        return columns_exist and not columns_have_nans
 
     @property
     def osc_angle(self) -> float:
@@ -497,6 +500,8 @@ class Experiment(ExperimentBase):
 
     def collect_run(self, run: Run) -> None:
         """Collect `run.steps` and place them in `self.steps` Queue."""
+        if run.has_beamshifts:
+            self.ctrl.beamshift.set(*run.table[['beamshift_x', 'beamshift_y']].iloc[0])
         with self.ctrl.beam.unblanked(delay=0.2):
             if run.continuous:
                 self._collect_scans(run=run)
