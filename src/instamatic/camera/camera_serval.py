05a55ced86cfe58e33f6b7ea308c8209f5c0e4a7
Daniel Tchoń <dantch@onet.eu>
2026-07-20 18:27:43 +0200

    Today's approach to camera rework, continue

--- src/instamatic/camera/camera_serval.py
+++ src/instamatic/camera/camera_serval.py
@@ -6,11 +6,10 @@ import json
 import logging
 import math
 import socket
-import threading
 from io import BytesIO
-from itertools import batched
 from math import prod
-from typing import Generator, Iterator, List, Optional, Sequence, Tuple, Union
+from threading import Thread
+from typing import Iterator, Optional, Sequence, Tuple, Union
 from urllib.parse import urlparse
 
 import numpy as np
@@ -26,10 +25,12 @@ logger = logging.getLogger(__name__)
 # 2. `java -jar .\server\serv-2.1.3.jar`
 # 3. launch `instamatic`
 
-class Ignore:  # sentinel object: informs `_get_images` to get a single image
-    pass
 
-IGNORE = Ignore()
+def _local_ip_for(remote_host: str, remote_port: int) -> str:
+    """Return the local IP used to reach (remote_host, remote_port)."""
+    with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as s:
+        s.connect((remote_host, remote_port))
+        return s.getsockname()[0]
 
 
 class CameraServal(CameraBase):
@@ -40,24 +41,60 @@ class CameraServal(CameraBase):
     MAX_EXPOSURE = 10.0
     BAD_EXPOSURE_MSG = 'Requested exposure exceeds native Serval support (>0-10s)'
 
-    def __init__(self, name='serval'):
+    def __init__(self, name='serval') -> None:
         """Initialize camera module."""
         super().__init__(name)
-        self.establish_connection()
-        self.dead_time = (
-            self.detector_config['TriggerPeriod'] - self.detector_config['ExposureTime']
-        )
+
+        self.tcp_dest: dict[str, str] = {}  # destination for serial movies
+        self.conn, self.tcp_listener = self.establish_connection()
+        dc = self.detector_config  # noqa: loaded from a camera/file.yaml
+        self.dead_time = dc['TriggerPeriod'] - dc['ExposureTime']
         self.movie_bufsize = 2 * 4 * prod(self.dimensions)
+        self.null_image = np.zeros(shape=self.dimensions, dtype=np.int32)
+
         logger.info(f'Camera {self.get_name()} initialized')
         atexit.register(self.release_connection)
 
-    @staticmethod
-    def _local_ip_for(remote_host: str, remote_port: int) -> str:
-        """Return the local IP used to reach (remote_host, remote_port)."""
-        # UDP "connect" does not send packets, but lets the OS choose interface/IP.
-        with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as s:
-            s.connect((remote_host, remote_port))
-            return s.getsockname()[0]
+    def establish_connection(self) -> tuple[ServalCamera, socket.socket]:
+        """Establish connection to the camera."""
+
+        http_url = urlparse(self.url)  # noqa - loaded from a camera/file.yaml
+        tcp_port = (http_url.port or 8080) + 1
+        local_ip = _local_ip_for(http_url.hostname, tcp_port)
+        tcp_base = f'tcp://connect@{local_ip}:{tcp_port}'
+        http_dest = {'Base': 'http://localhost', 'Format': 'tiff', 'Mode': 'count'}
+        self.tcp_dest = {'Base': tcp_base, 'Format': 'jsonimage', 'Mode': 'count'}
+
+        f = dict(bpc_file_path=self.bpc_file_path, dacs_file_path=self.dacs_file_path)
+        conn = ServalCamera()
+        conn.connect(http_url)
+        conn.set_chip_config_files(**f)
+        conn.set_detector_config(**self.detector_config)
+
+        tcp_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        tcp_listener.settimeout(5.0)
+        tcp_listener.bind(('0.0.0.0', tcp_port))
+        tcp_listener.listen(1)
+
+        self.conn.destination = {'Image': [http_dest]}
+        return conn, tcp_listener
+
+    def release_connection(self) -> None:
+        """Release the connection to the camera."""
+        self.conn.measurement_stop()
+        self.tcp_listener.close()
+        msg = f"Connection to camera '{self.get_name()}' released"
+        logger.info(msg)
+
+    def set_detector_config(self, **kwargs) -> None:
+        """Set detector config while infering about missing config params."""
+        if 'TriggerMode' not in kwargs:
+            tm = 'AUTOTRIGSTART_TIMERSTOP' if self.dead_time else 'CONTINUOUS'
+            kwargs['TriggerMode'] = tm
+        if 'TriggerPeriod' not in kwargs and 'ExposureTime' in kwargs:
+            kwargs['TriggerPeriod'] = kwargs['ExposureTime'] + self.dead_time
+        self.conn.set_detector_config(**kwargs)
 
     def get_image(self, exposure: Optional[float] = None, **kwargs) -> np.ndarray:
         """Image acquisition interface. If the exposure is not given, the
@@ -66,40 +103,27 @@ class CameraServal(CameraBase):
         exposure: `float` or `None`
             Exposure time in seconds.
         """
-        return self._get_images(n_frames=IGNORE, exposure=exposure, **kwargs)
-
-    def _get_images(
-        self,
-        n_frames: Union[int, Ignore],
-        exposure: Optional[float] = None,
-        **kwargs,
-    ) -> Union[np.ndarray, List[np.ndarray]]:
-        """General media acquisition dispatcher for other protected methods."""
-        n: int = 1 if n_frames is IGNORE else n_frames
         e: float = self.default_exposure if exposure is None else exposure
 
-        if n_frames == 0:  # single image is communicated via n_frames = IGNORE
-            return []
-
-        elif e < self.MIN_EXPOSURE:
+        if e < self.MIN_EXPOSURE:
             logger.warning('%s: %d', self.BAD_EXPOSURE_MSG, e)
-            if n_frames is IGNORE:
-                return self._get_image_null(exposure=e, **kwargs)
-            return [self._get_image_null(exposure=e, **kwargs) for _ in range(n)]
+            return self.null_image
 
         elif e > self.MAX_EXPOSURE:
             logger.warning('%s: %d', self.BAD_EXPOSURE_MSG, e)
-            n1 = math.ceil(e / self.MAX_EXPOSURE)
-            e = (e + self.dead_time) / n1 - self.dead_time
-            images = self._get_image_stack(n_frames=n * n1, exposure=e, **kwargs)
-            if n_frames is IGNORE:
-                return self._spliced_sum(images, exposure=e)
-            return [self._spliced_sum(i, exposure=e) for i in batched(images, n1)]
+            n = math.ceil(e / self.MAX_EXPOSURE)
+            e = (e + self.dead_time) / n - self.dead_time
+            images = list(self.get_movie(n_frames=n, exposure=e))
+            return self._spliced_sum(images, exposure=e)
 
-        else:  # if exposure is within limits
-            if n_frames is IGNORE:
-                return self._get_image_single(exposure=e, **kwargs)
-            return self._get_image_stack(n_frames=n, exposure=e, **kwargs)
+        logger.debug(f'Collecting a single image with exposure {exposure} s')
+        self.conn.set_detector_config(ExposureTime=exposure)
+        db = self.conn.dashboard
+        if db['Measurement'] is None or db['Measurement']['Status'] != 'DA_RECORDING':
+            self.conn.measurement_start()
+
+        response = self.conn.get_request('/measurement/image')
+        return tifffile.imread(BytesIO(response.content))
 
     def _spliced_sum(self, arrays: Sequence[np.ndarray], exposure: float) -> np.ndarray:
         """Sum a series of arrays while applying a dead time correction."""
@@ -108,31 +132,9 @@ class CameraServal(CameraBase):
         live_fraction = len(arrays) * exposure / total_exposure
         return (array_sum / live_fraction).astype(arrays[0].dtype)
 
-    def _get_image_null(self, **_) -> np.ndarray:
-        logger.debug('Creating a synthetic image with zero counts')
-        return np.zeros(shape=self.get_image_dimensions(), dtype=np.int32)
-
-    def _get_image_single(self, exposure: float, **_) -> np.ndarray:
-        """Request a single frame in the mode in a trigger collection mode."""
-        logger.debug(f'Collecting a single image with exposure {exposure} s')
-        self.conn.set_detector_config(
-            ExposureTime=exposure,
-            TriggerPeriod=exposure + self.dead_time,
-        )
-        db = self.conn.dashboard
-        if db['Measurement'] is None or db['Measurement']['Status'] != 'DA_RECORDING':
-            self.conn.measurement_start()
-        self.conn.trigger_start()
-        response = self.conn.get_request('/measurement/image')
-        return tifffile.imread(BytesIO(response.content))
-
-    def _get_image_stack(self, n_frames: int, exposure: float, **_) -> list[np.ndarray]:
-        """Get a series of images in a mode with minimal dead time."""
-        return list(self.get_movie(n_frames=n_frames, exposure=exposure))
-
     def get_movie(
         self, n_frames: int, exposure: Optional[float] = None, **kwargs
-    ) -> Generator[np.ndarray, None, None]:
+    ) -> Iterator[np.ndarray]:
         """Yield `n_frames` images received via a TCP stream with minimal dead
         time. If the exposure is not given, the default value is read from the
         config file. Binning is ignored.
@@ -142,81 +144,44 @@ class CameraServal(CameraBase):
         exposure: `float` or `None`
             Exposure time in seconds.
         """
-        logger.debug(f'Collecting {n_frames}-frame movie with exposure {exposure} s via TCP')
-        mode: str = 'AUTOTRIGSTART_TIMERSTOP' if self.dead_time else 'CONTINUOUS'
-        exposure: float = self.default_exposure if exposure is None else exposure
-
-        http_url = urlparse(self.conn.url)
-        tcp_port = (http_url.port or 8080) + 1
-        local_ip = self._local_ip_for(http_url.hostname, tcp_port)
-        tcp_base = f'tcp://connect@{local_ip}:{tcp_port}'
-        tcp_dest = {'Base': tcp_base, 'Format': 'jsonimage', 'Mode': 'count'}
+        logger.debug(f'Collecting {n_frames}-frame movie with {exposure=} s via TCP')
+        e: float = self.default_exposure if exposure is None else exposure
 
         self.conn.measurement_stop()
         previous_config = self.conn.detector_config
         previous_destination = self.conn.destination
+        self.conn.destination = {'Image': [self.tcp_dest]}
+        self.set_detector_config(ExposureTime=e, nTriggers=n_frames)
 
-        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        listener.settimeout(5.0)
-        try:
-            listener.bind(('0.0.0.0', tcp_port))
-            listener.listen(1)
-            self.conn.destination = {
-                'Image': [
-                    tcp_dest,
-                ]
-            }
-            self.conn.set_detector_config(
-                TriggerMode=mode,
-                ExposureTime=exposure,
-                TriggerPeriod=exposure + self.dead_time,
-                nTriggers=n_frames,
-            )
-            threading.Thread(target=self.conn.measurement_start, daemon=True).start()
+        def _get_movie_inner() -> Iterator[np.ndarray]:  # this runs on next():
             try:
-                sock, addr = listener.accept()
-            except socket.timeout:
-                raise TimeoutError('Serval failed to connect back within 5 seconds.')
-            with sock:
-                yield from ServalMovieDeserializer(sock, n_frames, self.movie_bufsize)
+                Thread(target=self.conn.measurement_start, daemon=True).start()
+                try:
+                    sock, _ = self.tcp_listener.accept()
+                except socket.timeout:
+                    raise TimeoutError('Serval failed to connect back within 5 seconds.')
+                with sock:
+                    bs = self.movie_bufsize
+                    yield from ServalMovieDeserializer(sock, n_frames, bs)
 
-        finally:
-            listener.close()
-            try:
-                self.conn.measurement_stop()
-            except Exception as e:
-                logger.error(f'Error stopping measurement: {e}')
-            try:
-                self.conn.destination = previous_destination
-                self.conn.set_detector_config(**previous_config)
-            except Exception as e:
-                logger.error(f'Error restoring config: {e}')
+            finally:
+                try:
+                    self.conn.measurement_stop()
+                except Exception as ex:
+                    logger.error(f'Error stopping measurement: {ex}')
+                try:
+                    self.conn.destination = previous_destination
+                    self.conn.set_detector_config(**previous_config)
+                except Exception as ex:
+                    logger.error(f'Error restoring config: {ex}')
+
+        return _get_movie_inner()
 
     def get_image_dimensions(self) -> Tuple[int, int]:
         """Get the binned dimensions reported by the camera."""
         binning = self.get_binning()
         dim_x, dim_y = self.get_camera_dimensions()
         return int(dim_x / binning), int(dim_y / binning)
-
-    def establish_connection(self) -> None:
-        """Establish connection to the camera."""
-        self.conn = ServalCamera()
-        self.conn.connect(self.url)
-        self.conn.set_chip_config_files(
-            bpc_file_path=self.bpc_file_path, dacs_file_path=self.dacs_file_path
-        )
-        self.conn.set_detector_config(**self.detector_config)
-
-        img_dest = {'Base': 'http://localhost', 'Format': 'tiff', 'Mode': 'count'}
-        self.conn.destination = {'Image': [img_dest]}
-
-    def release_connection(self) -> None:
-        """Release the connection to the camera."""
-        self.conn.measurement_stop()
-        name = self.get_name()
-        msg = f"Connection to camera '{name}' released"
-        logger.info(msg)
 
 
 class ServalMovieDeserializer(Iterator[np.ndarray]):
@@ -276,7 +241,6 @@ class ServalMovieDeserializer(Iterator[np.ndarray]):
 
 
 if __name__ == '__main__':
-
     cam = CameraServal()
     from IPython import embed
 
